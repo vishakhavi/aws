93a615c89872357c4a9dbc7eeda680e0fdc1d21f
vishakha vinayak <vinayak.v@northeastern.edu>
2021-11-14 23:35:49 -0500

    codedeploy agent

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,3 +5,9 @@ cd /home/ubuntu/
 sudo chown ubuntu:ubuntu webapp/
 cd webapp
 npm install
+sudo cp /home/ubuntu/webapp/config/aws_cw_config.json /opt/
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/aws_cw_config.json \
+    -s

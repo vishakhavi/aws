3062122db2e0ccd2f376eb194fc793a02e3c17ae
vishakha vinayak <vinayak.v@northeastern.edu>
2021-11-30 20:20:25 -0500

    email verification api

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -14,6 +14,7 @@ const sql = require("../models/db.js");
 const {parseHrtimeToSeconds} = require("../service/timer.service");
 const metricsService = require("../service/statsd.service");
 const loggerService = require("../service/logger.service");
+const { propfind } = require("superagent");
 
 // Create and Save a new Customer
 
@@ -63,6 +64,27 @@ exports.create = async (req, res) => {
 };
 
 // get all user data from the database.
+exports.verifyUser = (req,res) => {
+     console.log(req.protocol+":/"+req.get('host'));
+     if((req.protocol+"://"+req.get('host'))==("http://"+"prod.vishakhavinayak.me"))
+     {
+         console.log("Domain is matched. Information is from Authentic email");
+         if(req.query.email.length != 0)
+         {
+             console.log("email is verified");
+             res.end("<h1>Email "+email+" is been Successfully verified");
+         }
+         else
+         {
+             console.log("email is not verified");
+             res.end("<h1>Bad Request</h1>");
+         }
+     }
+     else
+     {
+         res.end("<h1>Request is from unknown source");
+     }
+}
 
 exports.getUserDetails = (req, res) => {
      

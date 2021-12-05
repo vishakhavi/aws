297f2930106f01f1a4d331f44139ea8343269af4
vishakha vinayak <vinayak.v@northeastern.edu>
2021-12-05 15:40:43 -0500

    ssl connection

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -139,6 +139,7 @@ exports.verifyUser = async (req, res) => {
                               let verifiedUser = await userService.updateVerifiedUser(req.query.email);
                               loggerService.info("User update successful"+verifiedUser);
                               loggerService.info("User verification status"+verifiedUser.verified);
+                              checkSslConnection();
                               res.status(204).send();
                          } catch (ex) {
                               loggerService.error("Exception at user.controller.js update user", ex);
